8f8b0c639d7391291c425dd150724fbc188906eb
OscarFHC <oscarhsnurene@gmail.com>
2018-05-16 01:16:35 -0400

    Edit the plotting script

--- EgPlots.R
+++ EgPlots.R
@@ -1,3 +1,5 @@
+### This is the R scripts for making 
+
 library(deSolve)
 library(ggplot)
 library(tidyverse)
@@ -15,7 +17,12 @@ State <- c(x = 0.1, y = 0.1)
 Time <- seq(0, 150, by = 1)
 
 out <- as.data.frame(ode(func = LotVmod, y = State, parms = Pars, times = Time)) %>%
-  mutate(x_dat = x + rnorm(1, 0, 1)) %>%
-  mutate(y_dat = y + rnorm(1, 0, 1))
+  mutate(x_rand = rnorm(151, 0, 0.05)) %>%
+  mutate(y_rand = rnorm(151, 0, 0.05)) %>%
+  mutate(x_dat = x + x_rand) %>%
+  mutate(y_dat = y + y_rand)
 
-out
+out %>%
+  ggplot() + 
+    geom_point(aes(x = time, y = x_dat), size = 1) + 
+    geom_line(aes(x = time, y = x), linetype = 2, size = 2)
